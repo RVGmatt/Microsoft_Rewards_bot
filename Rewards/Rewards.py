7bf3ea6fe2859fe169ec08afc8911feaa8b568e4
Matteo Maiullari <132582751+RVGmatt@users.noreply.github.com>
2026-05-06 20:05:53 +0200

    Add files via upload

---
+++ Rewards/Rewards.py
@@ -0,0 +1,30 @@
+#the purpose of this program is to automatically research 30 numbers in Microsoft Edge in order to earn points for Microsoft Rewards
+#version:: 1.0
+#created by Matteo Maiullari
+import pyautogui as pag
+import time
+
+print("Close this window to end the program execution")
+print("It is reccomended not to interact with the PC during the execution")
+
+time.sleep(3)
+
+pag.press('win')
+time.sleep(1)
+pag.write('edge')
+pag.press('enter')
+
+time.sleep(2)
+
+pag.hotkey('ctrl', 'l')
+
+pag.write('1')
+pag.press('enter')
+time.sleep(5)
+
+for i in range (2,30):
+    pag.hotkey('ctrl', 'l')
+    pag.write(str(i))
+    pag.press('space')
+    pag.press('enter')
+    time.sleep(5)
