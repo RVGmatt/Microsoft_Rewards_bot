f37497bf8559ff28c303cfaf2a57831da6390bc5
Matteo Maiullari <132582751+RVGmatt@users.noreply.github.com>
2026-05-12 10:47:59 +0200

    Update script to research 30 numbers in Edge

--- Rewards/Rewards.py
+++ Rewards/Rewards.py
@@ -1,5 +1,6 @@
 #the purpose of this program is to automatically research 30 numbers in Microsoft Edge in order to earn points for Microsoft Rewards
 #version:: 1.0
+#You can install the pyautogui library by typing "py -m pip install pyautogui" in the command prompt
 #created by Matteo Maiullari
 import pyautogui as pag
 import time
@@ -20,11 +21,11 @@ pag.hotkey('ctrl', 'l')
 
 pag.write('1')
 pag.press('enter')
-time.sleep(5)
+time.sleep(7)
 
-for i in range (2,30):
+for i in range (2,31):
     pag.hotkey('ctrl', 'l')
     pag.write(str(i))
     pag.press('space')
     pag.press('enter')
-    time.sleep(5)
+    time.sleep(7)
